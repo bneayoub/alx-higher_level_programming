b26e670f1f25cef1d421c24047cec5a1158935b2
bneayoub <bne.ayoub@gmail.com>
2023-06-06 17:44:08 +0100

    indents fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in range(0, len(str)):
         string = chr(ord(str[i]))
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-                string = chr(ord(str[i]) - ((ord('a')-ord('A'))))
+            string = chr(ord(str[i]) - ((ord('a')-ord('A'))))
         print('{}'.format(string), end='')
     print('\n', end='')
