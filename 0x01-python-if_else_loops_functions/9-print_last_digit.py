b621e38a238a33912f3ac31b95a5ccbbe1525aea
bneayoub <bne.ayoub@gmail.com>
2023-06-06 17:48:27 +0100

    last number

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print('{}'.format(number%10), end = '')
-    return number%10
+    print('{}'.format(abs(number) % 10), end='')
+    return abs(number) % 10
