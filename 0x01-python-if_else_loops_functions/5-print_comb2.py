cab86069d3331c8f13f7f91212326c0f57964a46
bneayoub <bne.ayoub@gmail.com>
2023-06-06 10:50:04 +0100

    Combs

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('0'),ord('9')+1):
-    for j in range(ord('0'),ord('9')+1):
-        if i != ord('9') or j != ord('9'):
-            print("{}{}".format(chr(i),chr(j)), end = ', ')
-        else:
-            print("{}{}".format(chr(i),chr(j)))
+for i in range(100):
+    code = "{:02d}".format(i)
+    if i != 99:
+        print(code, end=', ')
+    else:
+        print(code)
