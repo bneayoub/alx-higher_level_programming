993702f1394996ee42dc8048503b8e7e0d5529d2
bneayoub <bne.ayoub@gmail.com>
2023-06-08 20:13:46 +0100

    debugging

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-sum = 0
-for s in argv[1:]:
-    sum += int(s)
-print("{:d}".format(sum))
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    for s in argv[1:]:
+        sum += int(s)
+    print("{:d}".format(sum))
