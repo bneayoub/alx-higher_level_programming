23d4f89a00ef69a23a026ffaf4786d5f4e88841f
bneayoub <bne.ayoub@gmail.com>
2023-06-11 17:06:39 +0100

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,8 @@
 def multiple_returns(sentence):
     len = len(sentence)
     if len == 0:
-        resultult = (0, None)
-        return resultult
+        result = (0, None)
+        return result
     else:
         result = (len, sentence[0:1])
         return result
