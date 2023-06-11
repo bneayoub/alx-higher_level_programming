50dc13f6e61f7a7b1a852cb842d7ccd68155491b
bneayoub <bne.ayoub@gmail.com>
2023-06-11 17:01:35 +0100

    is palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *curr = *head, *tmp;
+
+	if (curr == NULL)
+		return (1);
+
+	if (tmp == NULL)
+		tmp = curr;
+
+	if (is_palindrome(&curr->next) && tmp->n == curr->n)
+	{
+		tmp = tmp->next;
+		return (1);
+	}
+	else
+		return (0);
+}
