03359131e23105882611eb05b2d158c388546d58
bneayoub <bne.ayoub@gmail.com>
2023-06-11 16:57:27 +0100

    maximum of list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def maxi_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
+
+    maxi = my_list[0]
+    for i in range(1, len(my_list)):
+        if maxi < my_list[i]:
+            maxi = my_list[i]
+        else:
+            continue
+    return maxi
