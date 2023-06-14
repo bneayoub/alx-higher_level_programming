9678150bed4ad74f28f00f9627c860b31aadc2d4
bneayoub <bne.ayoub@gmail.com>
2023-06-14 09:20:00 +0100

    lits

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -19,38 +19,3 @@ l.append(4)
 lib.print_python_list_info(l)
 l.pop()
 lib.print_python_list_info(l)
-julien@ubuntu:~/CPython$ python3 100-test_lists.py
-[*] Size of the Python List = 2
-[*] Allocated = 2
-Element 0: str
-Element 1: str
-[*] Size of the Python List = 1
-[*] Allocated = 2
-Element 0: str
-[*] Size of the Python List = 7
-[*] Allocated = 7
-Element 0: str
-Element 1: int
-Element 2: int
-Element 3: float
-Element 4: tuple
-Element 5: list
-Element 6: str
-[*] Size of the Python List = 0
-[*] Allocated = 0
-[*] Size of the Python List = 1
-[*] Allocated = 4
-Element 0: int
-[*] Size of the Python List = 5
-[*] Allocated = 8
-Element 0: int
-Element 1: int
-Element 2: int
-Element 3: int
-Element 4: int
-[*] Size of the Python List = 4
-[*] Allocated = 8
-Element 0: int
-Element 1: int
-Element 2: int
-Element 3: int
