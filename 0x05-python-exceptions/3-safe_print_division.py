cac3be347511fab1e0c09bc70b54bda73a269e7f
bneayoub <bne.ayoub@gmail.com>
2023-06-26 23:42:22 +0100

    DIVISION

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        division = a / b
+    except ZeroDivisionError:
+        division = None
+    finally:
+        print("Inside result: {}".format(division))
+    return division
