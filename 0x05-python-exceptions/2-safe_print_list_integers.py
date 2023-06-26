dd3231c5092129b3ddf459901f6f58633ac890f7
bneayoub <bne.ayoub@gmail.com>
2023-06-26 23:38:59 +0100

    prnt elems intss

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except(ValueError, TypeError):
+        except (ValueError, TypeError):
             continue
     print()
     return count
