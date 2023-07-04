e059befdfa842f43e3185829995ba226cae86e22
bneayoub <bne.ayoub@gmail.com>
2023-07-04 19:09:54 +0100

    tests driven

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+0-add_integer module supplies one function
+"""
+
+
+def add_integer(a, b):
+    """Adds 2 numbers"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
