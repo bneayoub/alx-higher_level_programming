80d602c41362bad357a4181b74eae5978199f12e
bneayoub <bne.ayoub@gmail.com>
2023-07-13 00:46:03 +0100

    Pycode style

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ Contains the inherits_from function
 
 def inherits_from(obj, a_class):
     """returns true if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
